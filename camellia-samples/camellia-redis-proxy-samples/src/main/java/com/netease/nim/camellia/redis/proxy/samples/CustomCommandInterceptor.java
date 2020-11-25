782d9818ecf2541de12640c4703b113394ab23e4
曹佳俊 <hzcaojiajun@corp.netease.com>
2020-11-25 20:41:08 +0800

    update sample

--- camellia-samples/camellia-redis-proxy-samples/src/main/java/com/netease/nim/camellia/redis/proxy/samples/CustomCommandInterceptor.java
+++ camellia-samples/camellia-redis-proxy-samples/src/main/java/com/netease/nim/camellia/redis/proxy/samples/CustomCommandInterceptor.java
@@ -5,21 +5,40 @@ import com.netease.nim.camellia.redis.proxy.command.async.CommandInterceptRespon
 import com.netease.nim.camellia.redis.proxy.command.async.CommandInterceptor;
 import com.netease.nim.camellia.redis.proxy.enums.RedisCommand;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.List;
+
 public class CustomCommandInterceptor implements CommandInterceptor {
 
     private static final CommandInterceptResponse KEY_TOO_LONG = new CommandInterceptResponse(false, "key too long");
     private static final CommandInterceptResponse VALUE_TOO_LONG = new CommandInterceptResponse(false, "value too long");
+    private static final CommandInterceptResponse FORBIDDEN = new CommandInterceptResponse(false, "forbidden");
 
     @Override
     public CommandInterceptResponse check(Command command) {
-        if (command.getRedisCommand() == RedisCommand.SET) {
-            byte[] key = command.getObjects()[1];
-            if (key.length > 256) {
-                return KEY_TOO_LONG;
+        SocketAddress clientSocketAddress = command.getCommandContext().getClientSocketAddress();
+        if (clientSocketAddress instanceof InetSocketAddress) {
+            String hostAddress = ((InetSocketAddress) clientSocketAddress).getAddress().getHostAddress();
+            if (hostAddress != null && hostAddress.equals("10.128.1.1")) {
+                return FORBIDDEN;
             }
-            byte[] value = command.getObjects()[2];
-            if (value.length > 1024 * 1024 * 5) {
-                return VALUE_TOO_LONG;
+        }
+        List<byte[]> keys = command.getKeys();
+        if (keys != null && !keys.isEmpty()) {
+            for (byte[] key : keys) {
+                if (key.length > 256) {
+                    return KEY_TOO_LONG;
+                }
+            }
+        }
+        if (command.getRedisCommand() == RedisCommand.SET) {
+            byte[][] objects = command.getObjects();
+            if (objects.length > 3) {
+                byte[] value = objects[2];
+                if (value.length > 1024 * 1024 * 5) {
+                    return VALUE_TOO_LONG;
+                }
             }
         }
         return CommandInterceptResponse.SUCCESS;
