138e861d8d969f06b726e9863cb7c9435e17ba6a
曹佳俊 <hzcaojiajun@corp.netease.com>
2020-12-09 10:06:30 +0800

    update spring redis sample

--- camellia-samples/camellia-spring-redis-samples/src/main/java/com/netease/nim/camellia/spring/redis/samples/Application.java
+++ camellia-samples/camellia-spring-redis-samples/src/main/java/com/netease/nim/camellia/spring/redis/samples/Application.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.util.SafeEncoder;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,7 +26,19 @@ public class Application implements InitializingBean {
 
     public void test() {
         template.opsForValue().set("k1", "v1", 10, TimeUnit.SECONDS);
-        logger.info("test success");
+        template.executePipelined((RedisCallback<Object>) connection -> {
+            connection.setEx(SafeEncoder.encode("k1"), 100, SafeEncoder.encode("v2"));
+            connection.setEx(SafeEncoder.encode("k12"), 100, SafeEncoder.encode("v12"));
+            return null;
+        });
+        String k1 = template.opsForValue().get("k1");
+        String k12 = template.opsForValue().get("k12");
+        if (k1 != null && k1.equals("v2") && k12 != null && k12.equals("v12")) {
+            logger.info("test success");
+        } else {
+            logger.info("test fail");
+        }
+
     }
 
     public static void main(String[] args) {
