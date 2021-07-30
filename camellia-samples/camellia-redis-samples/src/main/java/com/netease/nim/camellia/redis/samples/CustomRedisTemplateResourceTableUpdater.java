b8d49cdb0cc19954ee7573b713514fd9cf3739df
曹佳俊 <hzcaojiajun@corp.netease.com>
2021-07-30 14:40:35 +0800

    update samples/doc

--- camellia-samples/camellia-redis-samples/src/main/java/com/netease/nim/camellia/redis/samples/CustomRedisTemplateResourceTableUpdater.java
+++ camellia-samples/camellia-redis-samples/src/main/java/com/netease/nim/camellia/redis/samples/CustomRedisTemplateResourceTableUpdater.java
@@ -13,6 +13,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class CustomRedisTemplateResourceTableUpdater extends RedisTemplateResourceTableUpdater {
 
+    private String url = "redis://@127.0.0.1:6379";
+
     public CustomRedisTemplateResourceTableUpdater() {
         Executors.newSingleThreadScheduledExecutor()
                 .scheduleAtFixedRate(this::checkUpdate, 5, 5, TimeUnit.SECONDS);
@@ -21,13 +23,17 @@ public class CustomRedisTemplateResourceTableUpdater extends RedisTemplateResour
     @Override
     public ResourceTable getResourceTable() {
         //用于初始化ResourceTable
-        return ResourceTableUtil.simpleTable(new Resource("redis://@127.0.0.1:6379"));
+        return ResourceTableUtil.simpleTable(new Resource(url));
     }
 
     private void checkUpdate() {
         //从你的配置中心获取配置，或者监听配置的变更
-        ResourceTable resourceTable = ResourceTableUtil.simpleTable(new Resource("redis://pass123@127.0.0.1:6380"));
-        //如果配置发生了变更，则回调告诉CamelliaRedisTemplate有更新
-        invokeUpdateResourceTable(resourceTable);
+        String newUrl = "redis://pass123@127.0.0.1:6380";
+        if (!url.equals(newUrl)) {
+            //如果配置发生了变更，则回调告诉CamelliaRedisTemplate有更新
+            url = newUrl;
+            ResourceTable resourceTable = ResourceTableUtil.simpleTable(new Resource(url));
+            invokeUpdateResourceTable(resourceTable);
+        }
     }
 }
