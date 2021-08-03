380cb6c2c3263b5064c52fa00c7989f675313010
曹佳俊 <hzcaojiajun@corp.netease.com>
2021-08-03 10:45:34 +0800

    update samples

---
+++ camellia-samples/camellia-redis-proxy-samples/src/main/java/com/netease/nim/camellia/redis/proxy/samples/SimpleTest.java
@@ -0,0 +1,23 @@
+package com.netease.nim.camellia.redis.proxy.samples;
+
+import com.netease.nim.camellia.redis.proxy.command.async.interceptor.TroubleTrickKeysCommandInterceptor;
+import com.netease.nim.camellia.redis.proxy.util.CamelliaRedisProxyStarter;
+
+/**
+ * 不使用spring-boot-starter，手工启动一个proxy的方法
+ * Created by caojiajun on 2021/8/3
+ */
+public class SimpleTest {
+
+    public static void main(String[] args) {
+        //设置相关参数
+        CamelliaRedisProxyStarter.updatePort(6380);//设置proxy的端口
+        CamelliaRedisProxyStarter.updatePassword("pass123");//设置proxy的密码
+        CamelliaRedisProxyStarter.updateRouteConf("redis://@127.0.0.1:6379");//可以设置单个地址，也可以设置一个json去配置双写/分片等
+        CamelliaRedisProxyStarter.getServerProperties().setCommandInterceptorClassName(TroubleTrickKeysCommandInterceptor.class.getName());//设置拦截器
+        //其他参数设置....
+
+        //启动
+        CamelliaRedisProxyStarter.start();
+    }
+}
