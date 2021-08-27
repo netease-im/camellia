62b788b8a2176fb473fad5f63972fbaa7d6271ad
曹佳俊 <hzcaojiajun@corp.netease.com>
2021-08-27 10:45:22 +0800

    optimize CamelliaLoadingCache

--- camellia-tools/src/main/java/com/netease/nim/camellia/tools/CamelliaLoadingCache.java
+++ camellia-tools/src/main/java/com/netease/nim/camellia/tools/CamelliaLoadingCache.java
@@ -20,7 +20,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * 对比Caffeine的expireAfterWrite策略：
  * 当一个key很久没有访问过了，此时调用get方法，会触发一个缓存更新操作，但是如果缓存更新失败了会返回一个异常，相反CamelliaLoadingCache会返回一个旧值
  *
- * CamelliaLoadingCache在高tps下的性能不如Caffeine，请按需使用
+ * CamelliaLoadingCache适合于一些特定的使用场景，请按需使用
  *
  * Created by caojiajun on 2021/8/26
  */
@@ -45,9 +45,7 @@ public class CamelliaLoadingCache<K, V> {
         timeCache.start();
     }
 
-    private final ConcurrentLinkedHashMap<K, ValueGetter> cacheMap;
-    private final ConcurrentLinkedHashMap<K, Long> cacheRefreshTimeMap;
-    private final ConcurrentLinkedHashMap<K, Long> cacheHitTimeMap;
+    private final ConcurrentLinkedHashMap<K, ValueInfo> cacheMap;
     private final ConcurrentLinkedHashMap<K, AtomicBoolean> lockMap;
 
     private final CacheLoader<K, V> cacheLoader;
@@ -63,11 +61,7 @@ public class CamelliaLoadingCache<K, V> {
         this.cacheNull = cacheNull;
         this.concurrentMaxRetry = concurrentMaxRetry;
         this.concurrentSleepMs = concurrentSleepMs;
-        cacheMap = new ConcurrentLinkedHashMap.Builder<K, ValueGetter>()
-                .initialCapacity(initialCapacity).maximumWeightedCapacity(maxCapacity).build();
-        cacheRefreshTimeMap = new ConcurrentLinkedHashMap.Builder<K, Long>()
-                .initialCapacity(initialCapacity).maximumWeightedCapacity(maxCapacity).build();
-        cacheHitTimeMap = new ConcurrentLinkedHashMap.Builder<K, Long>()
+        cacheMap = new ConcurrentLinkedHashMap.Builder<K, ValueInfo>()
                 .initialCapacity(initialCapacity).maximumWeightedCapacity(maxCapacity).build();
         lockMap = new ConcurrentLinkedHashMap.Builder<K, AtomicBoolean>()
                 .initialCapacity(initialCapacity).maximumWeightedCapacity(maxCapacity).build();
@@ -157,12 +151,11 @@ public class CamelliaLoadingCache<K, V> {
      * @return 值
      */
     public V get(K key) {
-        ValueGetter valueGetter = cacheMap.get(key);
-        if (valueGetter != null) {
-            Long cacheRefreshTime = cacheRefreshTimeMap.get(key);
-            if (isCacheNotExpire(cacheRefreshTime)) {
-                cacheHitTimeMap.put(key, now);
-                return valueGetter.get();
+        ValueInfo valueInfo = cacheMap.get(key);
+        if (valueInfo != null) {
+            if (valueInfo.isCacheNotExpire()) {
+                valueInfo.updateCacheHitTime();
+                return valueInfo.get();
             }
         }
         try {
@@ -172,45 +165,44 @@ public class CamelliaLoadingCache<K, V> {
                 boolean lock = tryLock(key);
                 if (lock) {
                     try {
-                        V newValue = cacheLoader.load(key);
-                        if (newValue != null || cacheNull) {
-                            valueGetter = new ValueGetter(newValue);
-                            cacheMap.put(key, valueGetter);
-                            cacheRefreshTimeMap.put(key, now);
-                        }
-                        return newValue;
+                        return _load(key, valueInfo);
                     } finally {
                         releaseLock(key);
                     }
                 } else {
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(concurrentSleepMs);
-                    } catch (InterruptedException ignore) {
+                    TimeUnit.MILLISECONDS.sleep(concurrentSleepMs);
+                    if (valueInfo != null && valueInfo.isCacheNotExpire()) {
+                        valueInfo.updateCacheHitTime();
+                        return valueInfo.get();
                     }
-                    Long cacheRefreshTime = cacheRefreshTimeMap.get(key);
-                    if (isCacheNotExpire(cacheRefreshTime)) {
-                        valueGetter = cacheMap.get(key);
-                        if (valueGetter != null) {
-                            cacheHitTimeMap.put(key, now);
-                            return valueGetter.get();
-                        }
+                    valueInfo = cacheMap.get(key);
+                    if (valueInfo != null && valueInfo.isCacheNotExpire()) {
+                        valueInfo.updateCacheHitTime();
+                        return valueInfo.get();
                     }
                 }
             }
-            V newValue = cacheLoader.load(key);
-            if (newValue != null || cacheNull) {
-                valueGetter = new ValueGetter(newValue);
-                cacheMap.put(key, valueGetter);
-                cacheRefreshTimeMap.put(key, now);
-            }
-            return newValue;
+            return _load(key, valueInfo);
         } catch (Exception e) {
             //出现异常时返回旧值
-            if (valueGetter != null) {
-                return valueGetter.get();
+            if (valueInfo != null) {
+                return valueInfo.get();
             }
             throw new CamelliaLoadingCacheException(e);
         }
+    }
+
+    private V _load(K key, ValueInfo valueInfo) throws Exception {
+        V newValue = cacheLoader.load(key);
+        if (newValue != null || cacheNull) {
+            if (valueInfo == null) {
+                valueInfo = new ValueInfo();
+            }
+            valueInfo.updateValue(newValue);
+            valueInfo.updateCacheRefreshTime();
+            cacheMap.put(key, valueInfo);
+        }
+        return newValue;
     }
 
     private boolean tryLock(K key) {
@@ -233,44 +225,17 @@ public class CamelliaLoadingCache<K, V> {
         return lock;
     }
 
-    //缓存已经过期了
-    private boolean isCacheNotExpire(Long cacheRefreshTime) {
-        return cacheRefreshTime != null && now < cacheRefreshTime + expireMillis;
-    }
-
-    //缓存马上要过期了，
-    private boolean isCacheWillExpire(Long cacheRefreshTime) {
-        return cacheRefreshTime == null || now > cacheRefreshTime + expireMillis - expireMillis * 2 / 5;
-    }
-
-    //缓存是热的
-    private boolean isCacheHot(Long cacheHitTime) {
-        return cacheHitTime != null && now - cacheHitTime < expireMillis / 2;
-    }
-
     //重载缓存
     private void reload() {
-        Map<K, Long> map = new HashMap<>(cacheHitTimeMap);
-        for (Map.Entry<K, Long> entry : map.entrySet()) {
+        Map<K, ValueInfo> map = new HashMap<>(cacheMap);
+        for (Map.Entry<K, ValueInfo> entry : map.entrySet()) {
             K key = entry.getKey();
-            Long cacheHitTime = entry.getValue();
-            //如果这个key是热的，也就是刚刚访问过
-            boolean cacheHot = isCacheHot(cacheHitTime);
-            if (cacheHot) {
-                //则检查该key是否刚刚更新过
-                Long cacheRefreshTime = cacheRefreshTimeMap.get(key);
-                boolean cacheWillExpire = isCacheWillExpire(cacheRefreshTime);
-                //如果没有更新过，则后台线程主动更新一下缓存
-                if (cacheWillExpire) {
-                    try {
-                        V newValue = cacheLoader.load(key);
-                        if (newValue != null || cacheNull) {
-                            cacheMap.put(key, new ValueGetter(newValue));
-                            cacheRefreshTimeMap.put(key, now);
-                        }
-                    } catch (Exception e) {
-                        logger.error("reload error, key = {}", key, e);
-                    }
+            ValueInfo valueInfo = entry.getValue();
+            if (valueInfo.isCacheHot() && valueInfo.isCacheWillExpire()) {
+                try {
+                    _load(key, valueInfo);
+                } catch (Exception e) {
+                    logger.error("reload error, key = {}", key, e);
                 }
             }
         }
@@ -280,14 +245,37 @@ public class CamelliaLoadingCache<K, V> {
         V load(K key) throws Exception;
     }
 
-    private final class ValueGetter {
-        private final V value;
-        public ValueGetter(V value) {
-            this.value = value;
-        }
+    private final class ValueInfo {
+        V value;
+        long cacheRefreshTime;
+        long cacheHitTime;
 
         public V get() {
             return value;
+        }
+
+        public void updateValue(V value) {
+            this.value = value;
+        }
+
+        public void updateCacheRefreshTime() {
+            cacheRefreshTime = now;
+        }
+
+        public void updateCacheHitTime() {
+            cacheHitTime = now;
+        }
+
+        public boolean isCacheNotExpire() {
+            return now < cacheRefreshTime + expireMillis;
+        }
+
+        public boolean isCacheWillExpire() {
+            return now > cacheRefreshTime + expireMillis - expireMillis * 2 / 5;
+        }
+
+        public boolean isCacheHot() {
+            return now - cacheHitTime < expireMillis / 2;
         }
     }
 }
