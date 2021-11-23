6d1b1c5f80ca8918030a8205454c230f57ec5420
caojiajun <zj_caojiajun@163.com>
2021-11-23 13:51:15 +0800

    update ErrorLogCollector

--- camellia-redis-proxy/src/main/java/com/netease/nim/camellia/redis/proxy/util/ErrorLogCollector.java
+++ camellia-redis-proxy/src/main/java/com/netease/nim/camellia/redis/proxy/util/ErrorLogCollector.java
@@ -1,7 +1,7 @@
 package com.netease.nim.camellia.redis.proxy.util;
 
 import com.netease.nim.camellia.core.util.CamelliaThreadFactory;
-import com.netease.nim.camellia.redis.proxy.netty.ServerStatus;
+import com.netease.nim.camellia.redis.proxy.conf.ProxyDynamicConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,8 +19,10 @@ public class ErrorLogCollector {
 
     private static final Logger logger = LoggerFactory.getLogger(ErrorLogCollector.class);
 
-    private static final int MAX_MAP_SIZE = 10000;
-    private static final long PRINT_ERROR_TRACE_MIN_INTERVAL_MILLIS = 1000;
+    private static final String CONF_MAX_CACHE_SIZE = "error.log.collect.cache.max.size";
+    private static final int DEFAULT_MAX_CACHE_SIZE = 10000;
+    private static final String CONF_PRINT_ERROR_TRACE_MIN_INTERVAL_MILLIS = "error.log.collect.print.trace.min.interval.millis";
+    private static final long DEFAULT_PRINT_ERROR_TRACE_MIN_INTERVAL_MILLIS = 1000;
     private static ConcurrentHashMap<String, AtomicLong> logMap = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<String, AtomicLong> lastPrintStackTraceMap = new ConcurrentHashMap<>();
     static {
@@ -29,7 +31,7 @@ public class ErrorLogCollector {
     }
 
     public static void collect(Class clazz, String log) {
-        if (logMap.size() < MAX_MAP_SIZE) {
+        if (logMap.size() < ProxyDynamicConf.getInt(CONF_MAX_CACHE_SIZE, DEFAULT_MAX_CACHE_SIZE)) {
             AtomicLong count = CamelliaMapUtils.computeIfAbsent(logMap, clazz.getName() + ":" + log, k -> new AtomicLong(0L));
             count.incrementAndGet();
         } else {
@@ -39,10 +41,10 @@ public class ErrorLogCollector {
 
     public static void collect(Class clazz, String log, Throwable e) {
         collect(clazz, log);
-        if (lastPrintStackTraceMap.size() < MAX_MAP_SIZE) {
+        if (lastPrintStackTraceMap.size() < ProxyDynamicConf.getInt(CONF_MAX_CACHE_SIZE, DEFAULT_MAX_CACHE_SIZE)) {
             AtomicLong lastPrintStackTraceTime = CamelliaMapUtils.computeIfAbsent(lastPrintStackTraceMap, clazz.getName() + ":" + log, k -> new AtomicLong(0L));
             long lastTime = lastPrintStackTraceTime.get();
-            if (TimeCache.currentMillis - lastTime > PRINT_ERROR_TRACE_MIN_INTERVAL_MILLIS) {
+            if (TimeCache.currentMillis - lastTime > ProxyDynamicConf.getLong(CONF_PRINT_ERROR_TRACE_MIN_INTERVAL_MILLIS, DEFAULT_PRINT_ERROR_TRACE_MIN_INTERVAL_MILLIS)) {
                 boolean printTrace = lastPrintStackTraceTime.compareAndSet(lastTime, TimeCache.currentMillis);
                 if (printTrace) {
                     logger.error(clazz.getName() + ":" + log, e);
