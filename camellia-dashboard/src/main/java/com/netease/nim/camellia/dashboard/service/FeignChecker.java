260e41ae6a19563797ff89e48b981a499dd74aa3
caojiajun <zj_caojiajun@163.com>
2022-07-01 18:17:46 +0800

    fix FeignChecker without @Component

--- camellia-dashboard/src/main/java/com/netease/nim/camellia/dashboard/service/FeignChecker.java
+++ camellia-dashboard/src/main/java/com/netease/nim/camellia/dashboard/service/FeignChecker.java
@@ -2,10 +2,12 @@ package com.netease.nim.camellia.dashboard.service;
 
 import com.netease.nim.camellia.core.model.Resource;
 import com.netease.nim.camellia.feign.resource.FeignResourceUtils;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by caojiajun on 2022/3/8
  */
+@Component
 public class FeignChecker implements IResourceChecker {
     @Override
     public boolean check(String url) {
