51eddd13e6580670beea02ccf00d4bc2b40fbb79
caojiajun <zj_caojiajun@163.com>
2022-11-11 16:38:35 +0800

    CamelliaStatistics support p50/p90/p99

--- camellia-tools/src/main/java/com/netease/nim/camellia/tools/statistic/CamelliaStatistics.java
+++ camellia-tools/src/main/java/com/netease/nim/camellia/tools/statistic/CamelliaStatistics.java
@@ -12,10 +12,33 @@ public class CamelliaStatistics {
     private final LongAdder sum = new LongAdder();
     private final MaxValue maxValue = new MaxValue();
 
+    private final LongAdder[] distribute;
+
+    public CamelliaStatistics(int expectedMaxValue) {
+        distribute = new LongAdder[expectedMaxValue];
+        for (int i=0; i<expectedMaxValue; i++) {
+            distribute[i] = new LongAdder();
+        }
+    }
+
+    public CamelliaStatistics() {
+        this(5000);
+    }
+
     public void update(long value) {
         count.increment();
         sum.add(value);
         maxValue.update(value);
+        if (value < 0) return;
+        LongAdder distributeCounter;
+        if (value >= distribute.length) {
+            distributeCounter = distribute[distribute.length - 1];
+        } else {
+            distributeCounter = distribute[(int) value];
+        }
+        if (distributeCounter != null) {
+            distributeCounter.increment();
+        }
     }
 
     public CamelliaStatsData getStatsDataAndReset() {
@@ -23,7 +46,41 @@ public class CamelliaStatistics {
         long count = this.count.sumThenReset();
         long max = this.maxValue.getAndSet(0);
         double avg = (double) sum / count;
-        return new CamelliaStatsData(count, avg, max, sum);
+        long c = 0;
+        long p50Position = (long) (count * 0.5);
+        long p75Position = (long) (count * 0.75);
+        long p90Position = (long) (count * 0.90);
+        long p95Position = (long) (count * 0.95);
+        long p99Position = (long) (count * 0.99);
+        long p999Position = (long) (count * 0.999);
+        long p50 = -1;
+        long p75 = -1;
+        long p90 = -1;
+        long p95 = -1;
+        long p99 = -1;
+        long p999 = -1;
+        for (int i=0; i < distribute.length; i++) {
+            c += distribute[i].sumThenReset();
+            if (p50 == -1 && c >= p50Position) {
+                p50 = i;
+            }
+            if (p75 == -1 && c >= p75Position) {
+                p75 = i;
+            }
+            if (p90 == -1 && c >= p90Position) {
+                p90 = i;
+            }
+            if (p95 == -1 && c >= p95Position) {
+                p95 = i;
+            }
+            if (p99 == -1 && c >= p99Position) {
+                p99 = i;
+            }
+            if (p999 == -1 && c >= p999Position) {
+                p999 = i;
+            }
+        }
+        return new CamelliaStatsData(count, avg, max, sum, p50, p75, p90, p95, p99, p999);
     }
 
     public CamelliaStatsData getStatsData() {
@@ -31,7 +88,41 @@ public class CamelliaStatistics {
         long count = this.count.sum();
         long max = maxValue.get();
         double avg = (double) sum / count;
-        return new CamelliaStatsData(count, avg, max, sum);
+        long c = 0;
+        long p50Position = (long) (count * 0.5);
+        long p75Position = (long) (count * 0.75);
+        long p90Position = (long) (count * 0.90);
+        long p95Position = (long) (count * 0.95);
+        long p99Position = (long) (count * 0.99);
+        long p999Position = (long) (count * 0.999);
+        long p50 = -1;
+        long p75 = -1;
+        long p90 = -1;
+        long p95 = -1;
+        long p99 = -1;
+        long p999 = -1;
+        for (int i=0; i < distribute.length; i++) {
+            c += distribute[i].sum();
+            if (p50 == -1 && c >= p50Position) {
+                p50 = i;
+            }
+            if (p75 == -1 && c >= p75Position) {
+                p75 = i;
+            }
+            if (p90 == -1 && c >= p90Position) {
+                p90 = i;
+            }
+            if (p95 == -1 && c >= p95Position) {
+                p95 = i;
+            }
+            if (p99 == -1 && c >= p99Position) {
+                p99 = i;
+            }
+            if (p999 == -1 && c >= p999Position) {
+                p999 = i;
+            }
+        }
+        return new CamelliaStatsData(count, avg, max, sum, p50, p75, p90, p95, p99, p999);
     }
 
     private static class MaxValue {
