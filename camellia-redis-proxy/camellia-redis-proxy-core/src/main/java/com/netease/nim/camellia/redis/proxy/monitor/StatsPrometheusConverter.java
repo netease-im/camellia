d97c6ff789f68d519716fd3f16b2e84c41ae0ebc
tasszz2k <tasszz2k@gmail.com>
2023-01-06 18:04:47 +0700

    update(prometheus):#66: implementing rest plugin metrics

--- camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/monitor/StatsPrometheusConverter.java
+++ camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/monitor/StatsPrometheusConverter.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Stats converter into prometheus data layout
@@ -51,11 +53,11 @@ public class StatsPrometheusConverter {
             sb.append("# HELP redis_proxy_bid_bgroup Redis Proxy Bid Bgroup\n");
             sb.append("# TYPE redis_proxy_bid_bgroup gauge\n");
             for (BidBgroupStats bgroupStats : stats.getBidBgroupStatsList()) {
-                String bid1 = bgroupStats.getBid() == null ? DEFAULT : String.valueOf(bgroupStats.getBid());
-                String bgroup1 = bgroupStats.getBgroup() == null ? DEFAULT : bgroupStats.getBgroup();
+                String bid = bgroupStats.getBid() == null ? DEFAULT : String.valueOf(bgroupStats.getBid());
+                String bgroup = bgroupStats.getBgroup() == null ? DEFAULT : bgroupStats.getBgroup();
                 sb.append(String.format("redis_proxy_bid_bgroup{bid=\"%s\",bgroup=\"%s\",} %d%n",
-                        bid1,
-                        bgroup1,
+                        bid,
+                        bgroup,
                         bgroupStats.getCount()));
             }
 
@@ -63,11 +65,11 @@ public class StatsPrometheusConverter {
             sb.append("# HELP redis_proxy_detail Redis Proxy Detail\n");
             sb.append("# TYPE redis_proxy_detail counter\n");
             for (DetailStats detailStats : stats.getDetailStatsList()) {
-                String bid2 = detailStats.getBid() == null ? DEFAULT : String.valueOf(detailStats.getBid());
-                String bgroup2 = detailStats.getBgroup() == null ? DEFAULT : detailStats.getBgroup();
+                String bid = detailStats.getBid() == null ? DEFAULT : String.valueOf(detailStats.getBid());
+                String bgroup = detailStats.getBgroup() == null ? DEFAULT : detailStats.getBgroup();
                 sb.append(String.format("redis_proxy_detail{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %d%n",
-                        bid2,
-                        bgroup2,
+                        bid,
+                        bgroup,
                         detailStats.getCommand(),
                         detailStats.getCount()));
             }
@@ -98,17 +100,17 @@ public class StatsPrometheusConverter {
             sb.append("# HELP redis_proxy_bid_bgroup_spend_stats Redis Proxy Bid Bgroup Spend Stats\n");
             sb.append("# TYPE redis_proxy_bid_bgroup_spend_stats summary\n");
             for (BidBgroupSpendStats spendStats : stats.getBidBgroupSpendStatsList()) {
-                String bid3 = spendStats.getBid() == null ? DEFAULT : String.valueOf(spendStats.getBid());
-                String bgroup3 = spendStats.getBgroup() == null ? DEFAULT : spendStats.getBgroup();
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats_sum{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getAvgSpendMs() * spendStats.getCount()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats_count{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %d%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getCount()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.5\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getSpendMsP50()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.75\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getSpendMsP75()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.90\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getSpendMsP90()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.95\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getSpendMsP95()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.99\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getSpendMsP99()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.999\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getSpendMsP999()));
-                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"1\"} %f%n", bid3, bgroup3, spendStats.getCommand(), spendStats.getMaxSpendMs()));
+                String bid = spendStats.getBid() == null ? DEFAULT : String.valueOf(spendStats.getBid());
+                String bgroup = spendStats.getBgroup() == null ? DEFAULT : spendStats.getBgroup();
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats_sum{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getAvgSpendMs() * spendStats.getCount()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats_count{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %d%n", bid, bgroup, spendStats.getCommand(), spendStats.getCount()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.5\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getSpendMsP50()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.75\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getSpendMsP75()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.90\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getSpendMsP90()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.95\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getSpendMsP95()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.99\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getSpendMsP99()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"0.999\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getSpendMsP999()));
+                sb.append(String.format("redis_proxy_bid_bgroup_spend_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",quantile=\"1\"} %f%n", bid, bgroup, spendStats.getCommand(), spendStats.getMaxSpendMs()));
             }
 
             // ====resource.stats====
@@ -130,15 +132,26 @@ public class StatsPrometheusConverter {
             sb.append("# HELP redis_proxy_bid_bgroup_resource_command_stats Redis Proxy Bid Bgroup Resource Command Stats\n");
             sb.append("# TYPE redis_proxy_bid_bgroup_resource_command_stats counter\n");
             for (ResourceBidBgroupCommandStats commandStats : stats.getResourceBidBgroupCommandStatsList()) {
-                String bid4 = commandStats.getBid() == null ? DEFAULT : String.valueOf(commandStats.getBid());
-                String bgroup4 = commandStats.getBgroup() == null ? DEFAULT : commandStats.getBgroup();
+                String bid = commandStats.getBid() == null ? DEFAULT : String.valueOf(commandStats.getBid());
+                String bgroup = commandStats.getBgroup() == null ? DEFAULT : commandStats.getBgroup();
                 sb.append(String.format("redis_proxy_bid_bgroup_resource_command_stats{\"bid\"=\"%s\",bgroup=\"%s\",resource=\"%s\",command=\"%s\",} %d%n",
-                        bid4,
-                        bgroup4,
+                        bid,
+                        bgroup,
                         commandStats.getResource(), commandStats.getCommand(), commandStats.getCount()));
             }
 
             // ====route.conf====
+            sb.append("# HELP redis_proxy_route_conf Redis Proxy Route Conf\n");
+            sb.append("# TYPE redis_proxy_route_conf gauge");
+            for (RouteConf routeConf : stats.getRouteConfList()) {
+                String bid = routeConf.getBid() == null ? DEFAULT : String.valueOf(routeConf.getBid());
+                String bgroup = routeConf.getBgroup() == null ? DEFAULT : routeConf.getBgroup();
+                sb.append(String.format("redis_proxy_route_conf{bid=\"%s\",bgroup=\"%s\",routeConf=\"%s\",updateTime=\"%d\",} 1%n",
+                        bid,
+                        bgroup,
+                        routeConf.getResourceTable(),
+                        routeConf.getUpdateTime()));
+            }
             // ====redis.connect.stats====
             sb.append("# HELP redis_proxy_redis_connect_stats Redis Proxy Redis Connect Stats\n");
             sb.append("# TYPE redis_proxy_redis_connect_stats gauge\n");
@@ -163,10 +176,96 @@ public class StatsPrometheusConverter {
                 sb.append(String.format("redis_proxy_upstream_redis_spend_stats{redis_addr=\"%s\",quantile=\"1\"} %f%n", upstreamRedisSpendStats.getAddr(), upstreamRedisSpendStats.getMaxSpendMs()));
             }
 
-            // TODO: big.key.stats
-            // TODO: hot.key.stats
-            // TODO: hot.key.cache.stats
-            // TODO: slow.command.stats
+            // ====big.key.stats====
+            sb.append("# HELP redis_proxy_big_key_stats Redis Proxy Big Key Stats\n");
+            sb.append("# TYPE redis_proxy_big_key_stats counter\n");
+            List<BigKeyStats> bigKeyStatsList = stats.getBigKeyStatsList();
+            // BigKeyStatsMap: BigKeyStats group by bid, bgroup and command and count the number of keys
+            Map<BigKeyStatsKey, Long> bigKeyStatsMap = bigKeyStatsList.stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    bigKeyStats -> new BigKeyStatsKey(bigKeyStats.getBid(), bigKeyStats.getBgroup(), bigKeyStats.getCommand()),
+                                    Collectors.counting()
+                            )
+                    );
+            for (Map.Entry<BigKeyStatsKey, Long> entry : bigKeyStatsMap.entrySet()) {
+                BigKeyStatsKey bigKeyStatsKey = entry.getKey();
+                String bid = bigKeyStatsKey.getBid() == null ? DEFAULT : bigKeyStatsKey.getBid();
+                String bgroup = bigKeyStatsKey.getBgroup() == null ? DEFAULT : bigKeyStatsKey.getBgroup();
+                sb.append(String.format("redis_proxy_big_key_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %d%n",
+                        bid,
+                        bgroup,
+                        bigKeyStatsKey.getCommand(),
+                        entry.getValue()));
+            }
+
+            // ====hot.key.stats====
+            sb.append("# HELP redis_proxy_hot_key_stats Redis Proxy Hot Key Stats\n");
+            sb.append("# TYPE redis_proxy_hot_key_stats counter\n");
+            List<HotKeyStats> hotKeyStatsList = stats.getHotKeyStatsList();
+            // HotKeyStatsMap: HotKeyStats group by bid, bgroup and count the number of keys
+            Map<HotKeyStatsKey, Long> hotKeyStatsMap = hotKeyStatsList.stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    hotKeyStats -> new HotKeyStatsKey(hotKeyStats.getBid(), hotKeyStats.getBgroup()),
+                                    Collectors.counting()
+                            )
+                    );
+            for (Map.Entry<HotKeyStatsKey, Long> entry : hotKeyStatsMap.entrySet()) {
+                HotKeyStatsKey hotKeyStatsKey = entry.getKey();
+                String bid = hotKeyStatsKey.getBid() == null ? DEFAULT : hotKeyStatsKey.getBid();
+                String bgroup = hotKeyStatsKey.getBgroup() == null ? DEFAULT : hotKeyStatsKey.getBgroup();
+                sb.append(String.format("redis_proxy_hot_key_stats{bid=\"%s\",bgroup=\"%s\",} %d%n",
+                        bid,
+                        bgroup,
+                        entry.getValue()));
+            }
+
+            // ====hot.key.cache.stats====
+            sb.append("# HELP redis_proxy_hot_key_cache_stats Redis Proxy Hot Key Cache Stats\n");
+            sb.append("# TYPE redis_proxy_hot_key_cache_stats counter\n");
+            List<HotKeyCacheStats> hotKeyCacheStatsList = stats.getHotKeyCacheStatsList();
+            // HotKeyCacheStatsMap: HotKeyCacheStats group by bid, bgroup and count the number of keys
+            Map<HotKeyCacheStatsKey, Long> hotKeyCacheStatsMap = hotKeyCacheStatsList.stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    hotKeyCacheStats -> new HotKeyCacheStatsKey(hotKeyCacheStats.getBid(), hotKeyCacheStats.getBgroup()),
+                                    Collectors.counting()
+                            )
+                    );
+
+            for (Map.Entry<HotKeyCacheStatsKey, Long> entry : hotKeyCacheStatsMap.entrySet()) {
+                HotKeyCacheStatsKey hotKeyCacheStatsKey = entry.getKey();
+                String bid = hotKeyCacheStatsKey.getBid() == null ? DEFAULT : hotKeyCacheStatsKey.getBid();
+                String bgroup = hotKeyCacheStatsKey.getBgroup() == null ? DEFAULT : hotKeyCacheStatsKey.getBgroup();
+                sb.append(String.format("redis_proxy_hot_key_cache_stats{bid=\"%s\",bgroup=\"%s\",} %d%n",
+                        bid,
+                        bgroup,
+                        entry.getValue()));
+            }
+
+            // ====slow.command.stats====
+            sb.append("# HELP redis_proxy_slow_command_stats Redis Proxy Slow Command Stats\n");
+            sb.append("# TYPE redis_proxy_slow_command_stats counter\n");
+            List<SlowCommandStats> slowCommandStatsList = stats.getSlowCommandStatsList();
+            // SlowCommandStatsMap: SlowCommandStats group by bid, bgroup and command and count the number of keys
+            Map<SlowCommandStatsKey, Long> slowCommandStatsMap = slowCommandStatsList.stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    slowCommandStats -> new SlowCommandStatsKey(slowCommandStats.getBid(), slowCommandStats.getBgroup(), slowCommandStats.getCommand()),
+                                    Collectors.counting()
+                            )
+                    );
+            for (Map.Entry<SlowCommandStatsKey, Long> entry : slowCommandStatsMap.entrySet()) {
+                SlowCommandStatsKey slowCommandStatsKey = entry.getKey();
+                String bid = slowCommandStatsKey.getBid() == null ? DEFAULT : slowCommandStatsKey.getBid();
+                String bgroup = slowCommandStatsKey.getBgroup() == null ? DEFAULT : slowCommandStatsKey.getBgroup();
+                sb.append(String.format("redis_proxy_slow_command_stats{bid=\"%s\",bgroup=\"%s\",command=\"%s\",} %d%n",
+                        bid,
+                        bgroup,
+                        slowCommandStatsKey.getCommand(),
+                        entry.getValue()));
+            }
 
             // <<<<<<<END<<<<<<<
         } catch (Exception e) {
@@ -174,4 +273,99 @@ public class StatsPrometheusConverter {
         }
         return sb.toString();
     }
+
+
+    private static class BigKeyStatsKey extends BigKeyStats {
+
+        public BigKeyStatsKey(String bid, String bgroup, String command) {
+            super();
+            this.setBid(bid);
+            this.setBgroup(bgroup);
+            this.setCommand(command);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            BigKeyStatsKey that = (BigKeyStatsKey) o;
+            return Objects.equals(getBid(), that.getBid()) &&
+                    Objects.equals(getBgroup(), that.getBgroup()) &&
+                    Objects.equals(getCommand(), that.getCommand());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getBid(), getBgroup(), getCommand());
+        }
+    }
+
+    private static class HotKeyStatsKey extends HotKeyStats {
+        public HotKeyStatsKey(String bid, String bgroup) {
+            super();
+            this.setBid(bid);
+            this.setBgroup(bgroup);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            HotKeyStatsKey that = (HotKeyStatsKey) o;
+            return Objects.equals(getBid(), that.getBid()) &&
+                    Objects.equals(getBgroup(), that.getBgroup());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getBid(), getBgroup());
+        }
+    }
+
+    private static class HotKeyCacheStatsKey extends HotKeyCacheStats {
+        public HotKeyCacheStatsKey(String bid, String bgroup) {
+            super();
+            this.setBid(bid);
+            this.setBgroup(bgroup);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            HotKeyCacheStatsKey that = (HotKeyCacheStatsKey) o;
+            return Objects.equals(getBid(), that.getBid()) &&
+                    Objects.equals(getBgroup(), that.getBgroup());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getBid(), getBgroup());
+        }
+    }
+
+    private static class SlowCommandStatsKey extends SlowCommandStats {
+        public SlowCommandStatsKey(String bid, String bgroup, String command) {
+            super();
+            this.setBid(bid);
+            this.setBgroup(bgroup);
+            this.setCommand(command);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SlowCommandStatsKey that = (SlowCommandStatsKey) o;
+            return Objects.equals(getBid(), that.getBid()) &&
+                    Objects.equals(getBgroup(), that.getBgroup()) &&
+                    Objects.equals(getCommand(), that.getCommand());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getBid(), getBgroup(), getCommand());
+        }
+    }
 }
+
