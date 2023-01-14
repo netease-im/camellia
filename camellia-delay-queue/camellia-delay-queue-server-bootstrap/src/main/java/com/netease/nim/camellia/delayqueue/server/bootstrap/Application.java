09e0b8e45c3136a3e4bd2421b6c946d20abbcc0e
caojiajun <zj_caojiajun@163.com>
2023-01-14 20:48:12 +0800

    add camellia-delay-queue-server-bootstrap

---
+++ camellia-delay-queue/camellia-delay-queue-server-bootstrap/src/main/java/com/netease/nim/camellia/delayqueue/server/bootstrap/Application.java
@@ -0,0 +1,17 @@
+package com.netease.nim.camellia.delayqueue.server.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by caojiajun on 2022/7/21
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.netease.nim.camellia.delayqueue.server"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
