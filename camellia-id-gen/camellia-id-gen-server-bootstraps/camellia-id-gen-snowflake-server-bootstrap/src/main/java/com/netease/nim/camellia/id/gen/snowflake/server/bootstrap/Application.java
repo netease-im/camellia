1a3c5fe9a8ec2e9c0e11ba7b76e39119b227347d
caojiajun <zj_caojiajun@163.com>
2023-01-15 17:37:29 +0800

    add camellia-id-gen-server-bootstraps module

---
+++ camellia-id-gen/camellia-id-gen-server-bootstraps/camellia-id-gen-snowflake-server-bootstrap/src/main/java/com/netease/nim/camellia/id/gen/snowflake/server/bootstrap/Application.java
@@ -0,0 +1,17 @@
+package com.netease.nim.camellia.id.gen.snowflake.server.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by caojiajun on 2021/9/27
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.netease.nim.camellia.id.gen.springboot.snowflake"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
