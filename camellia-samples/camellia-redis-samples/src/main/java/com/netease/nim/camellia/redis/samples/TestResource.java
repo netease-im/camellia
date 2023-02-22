28e19e0a6ab85fd6f7158c2fdfa6957758ed2cbd
caojiajun <zj_caojiajun@163.com>
2023-02-22 16:34:10 +0800

    update TestResource

--- camellia-samples/camellia-redis-samples/src/main/java/com/netease/nim/camellia/redis/samples/TestResource.java
+++ camellia-samples/camellia-redis-samples/src/main/java/com/netease/nim/camellia/redis/samples/TestResource.java
@@ -26,6 +26,9 @@ public class TestResource {
 
         test("redis-cluster-slaves://@127.0.0.1:6379,127.0.0.2:6379", "redis-cluster-slaves://@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
         test("redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379", "redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
+        test("redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?", "redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
+        test("redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster", "redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
+        test("redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=", "redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
         test("redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=true", "redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=true");
         test("redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false", "redis-cluster-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
         test("redis-cluster-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379", "redis-cluster-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379?withMaster=false");
@@ -33,6 +36,8 @@ public class TestResource {
         test("redis-sentinel://@127.0.0.1:6379,127.0.0.2:6379/mymaster", "redis-sentinel://@127.0.0.1:6379,127.0.0.2:6379/mymaster");
         test("redis-sentinel://passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster", "redis-sentinel://passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster");
         test("redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster", "redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster");
+        test("redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?", "redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster");
+        test("redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db", "redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster");
         test("redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db=1", "redis-sentinel://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db=1");
 
         test("redis-sentinel-slaves://@127.0.0.1:6379,127.0.0.2:6379/mymaster", "redis-sentinel-slaves://@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false");
@@ -41,21 +46,32 @@ public class TestResource {
         test("redis-sentinel-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false", "redis-sentinel-slaves://passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false");
         test("redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster", "redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false");
         test("redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db=1", "redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false&db=1");
+        test("redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db=1&", "redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false&db=1");
+        test("redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db=1&withMaster", "redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false&db=1");
+        test("redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?db=1&withMaster=", "redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false&db=1");
         test("redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false&db=1", "redis-sentinel-slaves://user:passwd@127.0.0.1:6379,127.0.0.2:6379/mymaster?withMaster=false&db=1");
 
         test("redis-proxies://@127.0.0.1:6379,127.0.0.2:6379", "redis-proxies://@127.0.0.1:6379,127.0.0.2:6379");
         test("redis-proxies://passwd@127.0.0.1:6379,127.0.0.2:6379", "redis-proxies://passwd@127.0.0.1:6379,127.0.0.2:6379");
         test("redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379", "redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379");
+        test("redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379?", "redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379");
+        test("redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379?db", "redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379");
         test("redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379?db=1", "redis-proxies://user:passwd@127.0.0.1:6379,127.0.0.2:6379?db=1");
 
         test("redis-proxies-discovery://@proxyName", "redis-proxies-discovery://@proxyName");
         test("redis-proxies-discovery://passwd@proxyName", "redis-proxies-discovery://passwd@proxyName");
         test("redis-proxies-discovery://user:passwd@proxyName", "redis-proxies-discovery://user:passwd@proxyName");
+        test("redis-proxies-discovery://user:passwd@proxyName?", "redis-proxies-discovery://user:passwd@proxyName");
+        test("redis-proxies-discovery://user:passwd@proxyName?db", "redis-proxies-discovery://user:passwd@proxyName");
         test("redis-proxies-discovery://user:passwd@proxyName?db=1", "redis-proxies-discovery://user:passwd@proxyName?db=1");
 
         CamelliaRedisProxyContext.register(resource -> new JedisPool());
         test("camellia-redis-proxy://@proxyName", "camellia-redis-proxy://@proxyName");
         test("camellia-redis-proxy://passwd@proxyName", "camellia-redis-proxy://passwd@proxyName");
+        test("camellia-redis-proxy://passwd@proxyName?", "camellia-redis-proxy://passwd@proxyName");
+        test("camellia-redis-proxy://passwd@proxyName?bid", "camellia-redis-proxy://passwd@proxyName");
+        test("camellia-redis-proxy://passwd@proxyName?bid=", "camellia-redis-proxy://passwd@proxyName");
+        test("camellia-redis-proxy://passwd@proxyName?db=", "camellia-redis-proxy://passwd@proxyName");
         test("camellia-redis-proxy://passwd@proxyName?bid=1&bgroup=default", "camellia-redis-proxy://passwd@proxyName?bid=1&bgroup=default");
         test("camellia-redis-proxy://passwd@proxyName?db=1", "camellia-redis-proxy://passwd@proxyName?db=1");
         test("camellia-redis-proxy://passwd@proxyName?bid=1&bgroup=default&db=1", "camellia-redis-proxy://passwd@proxyName?bid=1&bgroup=default&db=1");
