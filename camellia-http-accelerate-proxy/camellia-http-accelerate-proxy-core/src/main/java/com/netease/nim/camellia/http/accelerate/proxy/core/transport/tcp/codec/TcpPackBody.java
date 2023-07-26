4c4ec44ff7da7f288c726aced60c18b8e3e9563c
caojiajun <zj_caojiajun@163.com>
2023-07-26 09:31:03 +0800

    remove no use code and file

--- camellia-http-accelerate-proxy/camellia-http-accelerate-proxy-core/src/main/java/com/netease/nim/camellia/http/accelerate/proxy/core/transport/tcp/codec/TcpPackBody.java
+++
@@ -1,9 +0,0 @@
-package com.netease.nim.camellia.http.accelerate.proxy.core.transport.tcp.codec;
-
-import com.netease.nim.camellia.codec.Marshallable;
-
-/**
- * Created by caojiajun on 2023/7/7
- */
-public abstract class TcpPackBody implements Marshallable {
-}
