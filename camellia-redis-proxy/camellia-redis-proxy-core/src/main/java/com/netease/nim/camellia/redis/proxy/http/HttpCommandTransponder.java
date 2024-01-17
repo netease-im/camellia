c8a7b215ac897a1aaf39d5936c51b52f55a7f6b8
caojiajun <zj_caojiajun@163.com>
2024-01-17 15:26:09 +0800

    feat: support fail monitor (#191)

--- camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/http/HttpCommandTransponder.java
+++ camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/http/HttpCommandTransponder.java
@@ -6,6 +6,8 @@ import com.netease.nim.camellia.redis.proxy.command.Command;
 import com.netease.nim.camellia.redis.proxy.command.CommandInvokeConfig;
 import com.netease.nim.camellia.redis.proxy.enums.RedisCommand;
 import com.netease.nim.camellia.redis.proxy.info.ProxyInfoUtils;
+import com.netease.nim.camellia.redis.proxy.monitor.CommandFailMonitor;
+import com.netease.nim.camellia.redis.proxy.monitor.ProxyMonitorCollector;
 import com.netease.nim.camellia.redis.proxy.netty.ChannelInfo;
 import com.netease.nim.camellia.redis.proxy.plugin.*;
 import com.netease.nim.camellia.redis.proxy.plugin.rewrite.RouteRewriteResult;
@@ -15,6 +17,7 @@ import com.netease.nim.camellia.redis.proxy.upstream.IUpstreamClientTemplate;
 import com.netease.nim.camellia.redis.proxy.upstream.IUpstreamClientTemplateFactory;
 import com.netease.nim.camellia.redis.proxy.upstream.connection.RedisConnectionHub;
 import com.netease.nim.camellia.redis.proxy.upstream.utils.CompletableFutureUtils;
+import com.netease.nim.camellia.redis.proxy.util.ErrorLogCollector;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -118,21 +121,25 @@ public class HttpCommandTransponder {
                 boolean pluginBreak = false;
                 ProxyRequest proxyRequest = new ProxyRequest(request.getDb(), command, factory);
                 for (ProxyPlugin plugin : requestPlugins) {
-                    ProxyPluginResponse pluginResponse = plugin.executeRequest(proxyRequest);
-                    if (pluginResponse.getRouteRewriterResult() != null) {
-                        rewriteResult = pluginResponse.getRouteRewriterResult();
-                    }
-                    if (!pluginResponse.isPass()) {
-                        if (!commandList.isEmpty()) {
-                            List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList);
-                            futures.addAll(flushFutures);
-                            commandList = new ArrayList<>();
+                    try {
+                        ProxyPluginResponse pluginResponse = plugin.executeRequest(proxyRequest);
+                        if (pluginResponse.getRouteRewriterResult() != null) {
+                            rewriteResult = pluginResponse.getRouteRewriterResult();
                         }
-                        CompletableFuture<Reply> pluginFuture = new CompletableFuture<>();
-                        pluginFuture.complete(pluginResponse.getReply());
-                        futures.add(pluginFuture);
-                        pluginBreak = true;
-                        break;
+                        if (!pluginResponse.isPass()) {
+                            if (!commandList.isEmpty()) {
+                                List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList, proxyPluginInitResp);
+                                futures.addAll(flushFutures);
+                                commandList = new ArrayList<>();
+                            }
+                            CompletableFuture<Reply> pluginFuture = new CompletableFutureWithPlugins(proxyPluginInitResp, command, true);
+                            pluginFuture.complete(pluginResponse.getReply());
+                            futures.add(pluginFuture);
+                            pluginBreak = true;
+                            break;
+                        }
+                    } catch (Exception e) {
+                        ErrorLogCollector.collect(HttpCommandTransponder.class, "executeRequest error", e);
                     }
                 }
                 if (pluginBreak) {
@@ -143,23 +150,25 @@ public class HttpCommandTransponder {
                     String bgroup = rewriteResult.getBgroup();
                     if (!Objects.equals(bid, channelInfo.getBid()) || !bgroup.equals(channelInfo.getBgroup())) {
                         if (!commandList.isEmpty()) {
-                            List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList);
+                            List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList, proxyPluginInitResp);
                             futures.addAll(flushFutures);
                             commandList = new ArrayList<>();
                         }
-                        List<CompletableFuture<Reply>> flush = flush(factory.getOrInitializeAsync(bid, bgroup), request.getDb(), Collections.singletonList(command));
+                        List<CompletableFuture<Reply>> flush = flush(factory.getOrInitializeAsync(bid, bgroup), request.getDb(), Collections.singletonList(command), proxyPluginInitResp);
                         futures.addAll(flush);
                     }
                     continue;
                 }
                 if (command.getRedisCommand() == RedisCommand.INFO) {
                     if (!commandList.isEmpty()) {
-                        List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList);
+                        List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList, proxyPluginInitResp);
                         futures.addAll(flushFutures);
                         commandList = new ArrayList<>();
                     }
                     CompletableFuture<Reply> infoReply = ProxyInfoUtils.getInfoReply(command, factory);
-                    futures.add(infoReply);
+                    CompletableFutureWithPlugins futureWithPlugins = new CompletableFutureWithPlugins(proxyPluginInitResp, command, false);
+                    infoReply.thenAccept(futureWithPlugins::complete);
+                    futures.add(futureWithPlugins);
                     continue;
                 }
                 commandList.add(command);
@@ -167,10 +176,10 @@ public class HttpCommandTransponder {
         }
         //flush
         if (commandList == null) {
-            futures = flush(templateFuture, request.getDb(), commands);
+            futures = flush(templateFuture, request.getDb(), commands, proxyPluginInitResp);
         } else {
             if (!commandList.isEmpty()) {
-                List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList);
+                List<CompletableFuture<Reply>> flushFutures = flush(templateFuture, request.getDb(), commandList, proxyPluginInitResp);
                 futures.addAll(flushFutures);
             }
         }
@@ -188,10 +197,10 @@ public class HttpCommandTransponder {
         return future;
     }
 
-    private List<CompletableFuture<Reply>> flush(CompletableFuture<IUpstreamClientTemplate> templateFuture, int db, List<Command> commands) {
+    private List<CompletableFuture<Reply>> flush(CompletableFuture<IUpstreamClientTemplate> templateFuture, int db, List<Command> commands, ProxyPluginInitResp pluginInitResp) {
         List<CompletableFuture<Reply>> futures = new ArrayList<>(commands.size());
-        for (int i = 0; i < commands.size(); i++) {
-            futures.add(new CompletableFuture<>());
+        for (Command command : commands) {
+            futures.add(new CompletableFutureWithPlugins(pluginInitResp, command, false));
         }
         templateFuture.thenAccept(template -> {
             if (template == null) {
@@ -215,6 +224,9 @@ public class HttpCommandTransponder {
         reply.setRequestId(request.getRequestId());
         reply.setCode(code);
         reply.setMsg(msg);
+        if (ProxyMonitorCollector.isMonitorEnable() && code != SUCCESS) {
+            CommandFailMonitor.incr("code=" + code + ",msg=" + msg);
+        }
         return reply;
     }
 
