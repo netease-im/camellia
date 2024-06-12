6041da209c750e4fd63ca65f02331b842475040e
caojiajun <zj_caojiajun@163.com>
2024-06-12 17:25:46 +0800

    optimize(kv): use array instead of map (#243)

--- camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/upstream/kv/cache/SlotLRUCache.java
+++ camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/upstream/kv/cache/SlotLRUCache.java
@@ -4,58 +4,54 @@ import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
 import com.netease.nim.camellia.redis.proxy.util.RedisClusterCRC16Utils;
 import com.netease.nim.camellia.tools.utils.BytesKey;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 
 /**
  * Created by caojiajun on 2024/6/12
  */
 public class SlotLRUCache<V> {
 
-    private final ConcurrentHashMap<Integer, ConcurrentLinkedHashMap<BytesKey, V>> map;
+    private final ConcurrentLinkedHashMap<BytesKey, V>[] array = new ConcurrentLinkedHashMap[RedisClusterCRC16Utils.SLOT_SIZE];
 
     public SlotLRUCache(int capacity) {
-        map = new ConcurrentHashMap<>();
         for (int i=0; i< RedisClusterCRC16Utils.SLOT_SIZE; i++) {
             ConcurrentLinkedHashMap<BytesKey, V> subMap = new ConcurrentLinkedHashMap.Builder<BytesKey, V>()
                     .initialCapacity(capacity)
                     .maximumWeightedCapacity(capacity)
                     .build();
-            map.put(i, subMap);
+            array[i] = subMap;
         }
     }
 
     public V get(int slot, BytesKey cacheKey) {
-        ConcurrentLinkedHashMap<BytesKey, V> subMap = map.get(slot);
+        ConcurrentLinkedHashMap<BytesKey, V> subMap = array[slot];
         return subMap.get(cacheKey);
     }
 
 
     public void put(int slot, BytesKey cacheKey, V value) {
-        ConcurrentLinkedHashMap<BytesKey, V> subMap = map.get(slot);
+        ConcurrentLinkedHashMap<BytesKey, V> subMap = array[slot];
         subMap.put(cacheKey, value);
     }
 
     public void remove(int slot, BytesKey cacheKey) {
-        ConcurrentLinkedHashMap<BytesKey, V> subMap = map.get(slot);
+        ConcurrentLinkedHashMap<BytesKey, V> subMap = array[slot];
         subMap.remove(cacheKey);
     }
 
     public void clear() {
-        for (Map.Entry<Integer, ConcurrentLinkedHashMap<BytesKey, V>> entry : map.entrySet()) {
-            entry.getValue().clear();
+        for (ConcurrentLinkedHashMap<BytesKey, V> subMap : array) {
+            subMap.clear();
         }
     }
 
     public void clear(int slot) {
-        ConcurrentLinkedHashMap<BytesKey, V> subMap = map.get(slot);
+        ConcurrentLinkedHashMap<BytesKey, V> subMap = array[slot];
         subMap.clear();
     }
 
     public void setCapacity(int capacity) {
-        for (Map.Entry<Integer, ConcurrentLinkedHashMap<BytesKey, V>> entry : map.entrySet()) {
-            entry.getValue().setCapacity(capacity);
+        for (ConcurrentLinkedHashMap<BytesKey, V> subMap : array) {
+            subMap.setCapacity(capacity);
         }
     }
 }
