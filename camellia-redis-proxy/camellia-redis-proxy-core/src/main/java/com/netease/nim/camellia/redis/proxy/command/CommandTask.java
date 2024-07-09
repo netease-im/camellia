cd0a645b324ca71931e4b07ea2168b85d12e3c9e
kerry <dingyong87@163.com>
2024-07-09 14:01:09 +0800

    feat(proxy): support ProxyPluginResponse redirect

--- camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/command/CommandTask.java
+++ camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/command/CommandTask.java
@@ -3,13 +3,13 @@ package com.netease.nim.camellia.redis.proxy.command;
 import com.netease.nim.camellia.redis.proxy.plugin.ProxyPlugin;
 import com.netease.nim.camellia.redis.proxy.plugin.ProxyPluginResponse;
 import com.netease.nim.camellia.redis.proxy.plugin.ProxyReply;
+import com.netease.nim.camellia.redis.proxy.plugin.rewrite.RouteRewriteResult;
 import com.netease.nim.camellia.redis.proxy.reply.Reply;
 import com.netease.nim.camellia.redis.proxy.util.ErrorLogCollector;
 
 import java.util.List;
 
 /**
- *
  * Created by caojiajun on 2019/12/12.
  */
 public class CommandTask {
@@ -18,6 +18,34 @@ public class CommandTask {
     private final Command command;
     private final List<ProxyPlugin> plugins;
     private Reply reply;
+
+    private boolean isRedirect = false;
+
+    public boolean isRedirect() {
+        return isRedirect;
+    }
+
+    public void setRedirect(boolean redirect) {
+        isRedirect = redirect;
+    }
+
+    private long bidRedirect;
+
+    public long getBidRedirect() {
+        return bidRedirect;
+    }
+
+    private String bgroupRedirect;
+
+    public String getBgroupRedirect() {
+        return bgroupRedirect;
+    }
+
+    private boolean isSkipPlugins = false;
+
+    public void setSkipPlugins(boolean skipPlugins) {
+        isSkipPlugins = skipPlugins;
+    }
 
     public CommandTask(CommandTaskQueue taskQueue, Command command, List<ProxyPlugin> plugins) {
         this.command = command;
@@ -31,12 +59,19 @@ public class CommandTask {
 
     public void replyCompleted(Reply reply, boolean fromPlugin) {
         try {
-            if (plugins != null && !plugins.isEmpty()) {
+            if (!isSkipPlugins && plugins != null && !plugins.isEmpty()) {
                 ProxyReply proxyReply = new ProxyReply(command, reply, fromPlugin);
                 for (ProxyPlugin plugin : plugins) {
                     try {
                         ProxyPluginResponse response = plugin.executeReply(proxyReply);
                         if (!response.isPass()) {
+                            this.isRedirect = response.isRedirect();
+                            if (this.isRedirect) {
+                                RouteRewriteResult routeRewriterResult = response.getRouteRewriterResult();
+                                this.bidRedirect = routeRewriterResult.getBid();
+                                this.bgroupRedirect = routeRewriterResult.getBgroup();
+                            }
+
                             this.reply = response.getReply();
                             this.taskQueue.callback();
                             return;
