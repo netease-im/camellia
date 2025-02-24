cd14c1a6b982a2c08c921059b267fca075d1e13e
caojiajun <zj_caojiajun@163.com>
2025-02-24 20:09:20 +0800

    feat(proxy): read only plugin support config by telnant (#389)

--- camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/plugin/permission/ReadOnlyProxyPlugin.java
+++ camellia-redis-proxy/camellia-redis-proxy-core/src/main/java/com/netease/nim/camellia/redis/proxy/plugin/permission/ReadOnlyProxyPlugin.java
@@ -1,7 +1,10 @@
 package com.netease.nim.camellia.redis.proxy.plugin.permission;
 
+import com.netease.nim.camellia.redis.proxy.command.CommandContext;
+import com.netease.nim.camellia.redis.proxy.conf.ProxyDynamicConf;
 import com.netease.nim.camellia.redis.proxy.enums.RedisCommand;
 import com.netease.nim.camellia.redis.proxy.plugin.*;
+import com.netease.nim.camellia.redis.proxy.util.ErrorLogCollector;
 
 /**
  * Created by caojiajun on 2024/1/17
@@ -27,10 +30,22 @@ public class ReadOnlyProxyPlugin implements ProxyPlugin {
 
     @Override
     public ProxyPluginResponse executeRequest(ProxyRequest request) {
-        RedisCommand redisCommand = request.getCommand().getRedisCommand();
-        if (redisCommand.getType() != RedisCommand.Type.READ) {
-            return FORBIDDEN;
+        try {
+            CommandContext commandContext = request.getCommand().getCommandContext();
+            Long bid = commandContext.getBid();
+            String bgroup = commandContext.getBgroup();
+            boolean enable = ProxyDynamicConf.getBoolean("read.only.plugin.enable", bid, bgroup, true);
+            if (!enable) {
+                return ProxyPluginResponse.SUCCESS;
+            }
+            RedisCommand redisCommand = request.getCommand().getRedisCommand();
+            if (redisCommand.getType() != RedisCommand.Type.READ) {
+                return FORBIDDEN;
+            }
+            return ProxyPluginResponse.SUCCESS;
+        } catch (Exception e) {
+            ErrorLogCollector.collect(ReadOnlyProxyPlugin.class, "read only plugin execute error", e);
+            return ProxyPluginResponse.SUCCESS;
         }
-        return ProxyPluginResponse.SUCCESS;
     }
 }
