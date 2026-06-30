32349ddb7dc384d15c2892944019155d65d7cba5
caojiajun <zj_caojiajun@163.com>
2026-06-30 09:47:21 +0800

    Bump to 1.4.3-SNAPSHOT

--- camellia-core/src/main/java/com/netease/nim/camellia/core/constant/CamelliaVersion.java
+++ camellia-core/src/main/java/com/netease/nim/camellia/core/constant/CamelliaVersion.java
@@ -5,5 +5,5 @@ package com.netease.nim.camellia.core.constant;
  */
 public class CamelliaVersion {
 
-    public static final String version = "1.4.2";
+    public static final String version = "1.4.3-SNAPSHOT";
 }
